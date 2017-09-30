934879a896eb992fdae7eac0986771a7c8043255
hiro1151 <hiro.gitcut77@gmail.com>
2017-09-30 15:41:49 +0000

    kadaitw-microposts

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,14 @@ class FavoritesController < ApplicationController
   def create
     micropost = Micropost.find(params[:micropost_id])
     current_user.favorite_micropost(micropost)
-    flash[:success] = 'ユーザーをお気に入りしました。'
+    flash[:success] = '投稿をお気に入りしました。'
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite_micropost(micropost)
-    flash[:success] = 'ユーザーのお気に入りを解除しました。'
+    flash[:success] = '投稿のお気に入りを解除しました。'
     redirect_back(fallback_location: root_path)
   end
 end
